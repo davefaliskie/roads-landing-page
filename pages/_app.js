1c9ee5f45a6f0556a5210f853f206b5767d2f19d
Dave Faliskie <davefaliskie@gmail.com>
2023-03-29 14:04:37 -0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={montserrat.className}>
+    <Component {...pageProps} />
+  </main>
 }
