31758dc3bcb2c9aadac82d384400b0b4f938c5ac
Dave Faliskie <davefaliskie@gmail.com>
2023-03-30 15:15:12 -0300

    display the 3 phones & placeholder txt.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import styles from '@/styles/Home.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
 import ConvertkitEmailForm from '@/components/ConvertkitEmailForm';
+import NodeOverlay from '@/components/NodeOverlay';
 
 export default function Home() {
   return (
@@ -14,9 +15,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Image src="/Roads.png" width={300} height={300} alt="Roads Logo" />
+        {/* <Image src="/Roads.png" width={300} height={300} alt="Roads Logo" />
         <h1>Roads</h1>
-        <ConvertkitEmailForm />
+        <ConvertkitEmailForm /> */}
+        
+        <NodeOverlay />
+        <div id={styles.section_headline}>
+          <div className='text-center text-light'>
+            <h1>Interact with audio</h1>
+            <p>Record short podcasts on any topic and receive audio comments from other users.</p>
+          </div>
+
+          <div className='text-center mt-5'>
+            <Image 
+              src="/images/phone1.png" 
+              alt="Screenshot of the app"
+              height={360}
+              width={180}
+              className={styles.phone_shadow}
+            />
+            <Image 
+              src="/images/phone1.png" 
+              alt="Screenshot of the app"
+              height={420}
+              width={210}
+              className={`${styles.phone_shadow} mx-4`}
+            />
+            <Image 
+              src="/images/phone1.png" 
+              alt="Screenshot of the app"
+              height={360}
+              width={180}
+              className={styles.phone_shadow}
+            />
+          </div>
+        </div>
       </main>
     </>
   );
