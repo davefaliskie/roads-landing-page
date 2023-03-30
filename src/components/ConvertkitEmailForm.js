27ab289696204d675416155e6dfabcf8909612c4
Dave Faliskie <davefaliskie@gmail.com>
2023-03-29 21:35:04 -0300

    Setup form with convertkit

---
+++ src/components/ConvertkitEmailForm.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class ConvertkitEmailForm extends Component {
+  state = {
+    message: '',
+    email: ''
+  }
+
+  emailHandler = (e) => {
+    let updatedEmail = e.target.value;
+    this.setState({ email: updatedEmail });
+  }
+
+  subscribeUser = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/ConvertkitSubscribe', {
+      body: JSON.stringify({email: this.state.email}),
+      headers: {'Content-Type': 'application/json; charset=utf-8'},
+      method: 'POST',
+    });
+
+    const json_res = await res.json();
+
+    this.setState({
+      message: json_res.message,
+      email: ''
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.subscribeUser}>
+        <div className="input-group">
+          <input 
+            id="newsletter-input"
+            type="email"
+            name="email"
+            className="form-control shadow-none" 
+            placeholder="Enter your email" 
+            aria-label="Enter Email Address" 
+            aria-describedby="nnewsletter-btn"
+            value={this.state.email}
+            onChange={this.emailHandler}
+            required
+            autoCapitalize="off"
+            autoCorrect="off"
+            />
+          <button 
+            type="submit" 
+            id="newsletter-btn" 
+            className="btn btn-primary btn-lg font-title"
+            value=""
+            name="subscribe"
+          >
+            NOTIFY ME
+          </button>
+        </div>
+        <p id="newsletter-message" className='text-center'>{this.state.message}</p>
+
+      </form>
+    );
+  }
+}
+
+export default ConvertkitEmailForm;
---
+++ src/pages/api/ConvertkitSubscribe.js
@@ -0,0 +1,41 @@
+
+const API_KEY = process.env.CONVERTKIT_API_KEY;
+const FORM_ID = process.env.CONVERTKIT_FORM_ID;
+const BASE_URL = 'https://api.convertkit.com/v3';
+const error_message = `There was a problem, please try again.`
+const success_message = `You have successfully subscribed!`
+
+async function convertkitSubscribeHandler(req, res) {
+  const email = req.body.email;
+
+  if (!email) {
+    return res.status(400).json({ message: 'Email is required' });
+  }
+
+  try {
+    const url = [BASE_URL, `forms`, FORM_ID, 'subscribe'].join('/');    
+    const data = {
+      api_key: API_KEY,
+      email: email,
+    };
+
+    const response = await fetch(
+      url,
+      {
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        method: 'POST',
+      }
+    );
+
+    if (response.status == 200) {
+      return res.status(200).json({ message: success_message });
+    } else {
+      return res.status(500).json({ message: error_message });
+    }    
+  } catch (error) {
+    return res.status(500).json({ message: error_message });
+  }
+};
+
+export default convertkitSubscribeHandler;
