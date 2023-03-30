75d6822cbd2f91ef0415b6839a29ba2f2d964c98
Dave Faliskie <davefaliskie@gmail.com>
2023-03-30 15:14:37 -0300

    Make background node component

---
+++ src/components/NodeOverlay.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/components/NodeOverlay.module.scss';
+
+class NodeOverlay extends Component {
+  render() {
+    return (
+      <>
+      <Image 
+            src="/images/node.svg" 
+            width={300} 
+            height={300} 
+            alt="Background Circle Image"
+            className={styles.node_one}
+          />
+      <Image 
+            src="/images/node-light.svg" 
+            width={300} 
+            height={300} 
+            alt="Background Circle Image"
+            className={styles.node_two}
+          />
+      </>
+    );
+  }
+}
+
+export default NodeOverlay;
