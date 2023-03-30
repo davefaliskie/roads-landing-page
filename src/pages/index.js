7fc858cbf63a21d4004a959e835ae449ca43bb54
Dave Faliskie <davefaliskie@gmail.com>
2023-03-30 18:23:06 -0300

    basic setup for single feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,59 +20,88 @@ export default function Home() {
         <ConvertkitEmailForm /> */}
         
         <NodeOverlay />
+        
+        <div id={styles.section_headline_container}>
+          <div id={styles.section_headline}>
+            <span className={styles.vLine1}>
+              <Image 
+                src="/images/node-bright.svg" 
+                width={15} 
+                height={15} 
+                alt="Background Circle Image"
+                className={styles.node_sm1}
+              />
+            </span>
+            <span className={styles.vLine2}></span>
+            <span className={styles.vLine3}></span>
+            <span className={styles.vLine4}></span>
+            <span className={styles.vLine5}></span>
+            <span className={styles.vLine6}>
+              <Image 
+                src="/images/node-bright.svg" 
+                width={15} 
+                height={15} 
+                alt="Background Circle Image"
+                className={styles.node_sm2}
+              />
+            </span>
 
-        <div id={styles.section_headline}>
-          <span className={styles.vLine1}>
-            <Image 
-              src="/images/node-bright.svg" 
-              width={15} 
-              height={15} 
-              alt="Background Circle Image"
-              className={styles.node_sm1}
-            />
-          </span>
-          <span className={styles.vLine2}></span>
-          <span className={styles.vLine3}></span>
-          <span className={styles.vLine4}></span>
-          <span className={styles.vLine5}></span>
-          <span className={styles.vLine6}>
-            <Image 
-              src="/images/node-bright.svg" 
-              width={15} 
-              height={15} 
-              alt="Background Circle Image"
-              className={styles.node_sm2}
-            />
-          </span>
+            <div className='text-center text-light'>
+              <h1>Interact with audio</h1>
+              <p>Record short podcasts on any topic and receive audio comments from other users.</p>
+            </div>
 
-          <div className='text-center text-light'>
-            <h1>Interact with audio</h1>
-            <p>Record short podcasts on any topic and receive audio comments from other users.</p>
+            <div className={`${styles.cover_imgs_container} text-center`}>
+              <Image 
+                src="/images/phone1.png" 
+                alt="Screenshot of the app"
+                height={400}
+                width={200}
+                className={styles.phone_shadow}
+              />
+              <Image 
+                src="/images/phone1.png" 
+                alt="Screenshot of the app"
+                height={460}
+                width={230}
+                className={`${styles.phone_shadow} mx-4`}
+              />
+              <Image 
+                src="/images/phone1.png" 
+                alt="Screenshot of the app"
+                height={400}
+                width={200}
+                class={styles.phone_shadow}
+              />
+            </div>
           </div>
+        </div>
 
-          <div className={`${styles.cover_imgs_container} text-center`}>
-            <Image 
-              src="/images/phone1.png" 
-              alt="Screenshot of the app"
-              height={400}
-              width={200}
-              className={styles.phone_shadow}
-            />
-            <Image 
-              src="/images/phone1.png" 
-              alt="Screenshot of the app"
-              height={460}
-              width={230}
-              className={`${styles.phone_shadow} mx-4`}
-            />
-            <Image 
-              src="/images/phone1.png" 
-              alt="Screenshot of the app"
-              height={400}
-              width={200}
-              className={styles.phone_shadow}
-            />
+        <div id={styles.section_features}>
+          <h2 class="my-5 text-center">Features</h2>
+
+          <div class="container">
+            <div class="row">
+              <div class={`${styles.backsplash_container} col-7 text-center`} >
+
+                <div class={`${styles.backsplash} mx-auto`}></div>
+                <Image 
+                  src="/images/phone1.png" 
+                  alt="Screenshot of the app"
+                  height={400}
+                  width={200}
+                  class={`${styles.phone_shadow} ${styles.backsplash_image}`}
+                />
+              </div>
+
+              <div class='col my-auto'>
+                <h3>Feature #1</h3>
+                <p>This feature description will be max two sentances. and will highlight what is shown in the image.</p>
+              </div>
+            </div>
+
           </div>
+          
         </div>
       </main>
     </>
