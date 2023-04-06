80ac96b8b321049d7e06f9ede9317d049af1aaac
Dave Faliskie <davefaliskie@gmail.com>
2023-04-06 10:56:53 -0400

    Move meta info to document level

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,28 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* PRIMARY META */}
         <title>Roads - Discover the Future of Audio</title>
-        <meta name="title" content="Roads - Discover the Future of Audio" />
-        <meta name="description" content="Record, comment, and share your voice on the first app that connects people through dynamic conversations" />
-
-        {/*  Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://roadsaudio.com" />
-        <meta property="og:title" content="Roads - Discover the Future of Audio" />
-        <meta property="og:description" content="Record, comment, and share your voice on the first app that connects people through dynamic conversations" />
-        <meta property="og:image" content="https://roadsaudioassets.s3.amazonaws.com/Roads+Cover+Image.png" />
-
-        {/*  Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://roadsaudio.com" />
-        <meta property="twitter:title" content="Roads - Discover the Future of Audio" />
-        <meta property="twitter:description" content="Record, comment, and share your voice on the first app that connects people through dynamic conversations" />
-        <meta property="twitter:image" content="https://roadsaudioassets.s3.amazonaws.com/Roads+Cover+Image.png"></meta>
       </Head>
       <main className={styles.main}>        
         <NodeOverlay />
