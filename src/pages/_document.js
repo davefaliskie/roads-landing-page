b2995307baac626c00e8005b637c837e578148c5
Dave Faliskie <davefaliskie@gmail.com>
2023-04-06 10:29:36 -0400

    Add Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GTAG_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+      </Head>
       <body>
         <Main />
         <NextScript />
