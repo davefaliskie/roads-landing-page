4cb85a6628ea169f9fc0f6eda1715de6484e6e6a
Dave Faliskie <davefaliskie@gmail.com>
2023-08-09 23:20:27 -0500

    move viewport meta tag from _document to _app

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/favicon.ico" />
 
