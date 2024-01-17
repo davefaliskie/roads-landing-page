9bcc62386e4161bad79285192fc14505dc2b70d8
Dave Faliskie <davefaliskie@gmail.com>
2024-01-17 21:26:11 +0100

    Setup delete-account page.

--- src/utils/metaConfig.js
+++ src/utils/metaConfig.js
@@ -4,6 +4,11 @@ const metaConfig = {
     description: 'Record, comment, and share your voice on the app that connects people through dynamic conversations',
     image: 'https://roadsaudioassets.s3.amazonaws.com/Roads_Cover_V2.png',
   },
+  '/delete-account': {
+    title: 'The Evolution of Audio: How to delete your account',
+    description: 'See how to delete your account and data from Roads.',
+    image: 'https://roadsaudioassets.s3.amazonaws.com/Roads_Cover_V2.png',
+  },
   '/blogs/the-evolution-of-audio-from-radio-to-roads': {
     title: 'The Evolution of Audio: From Radio to Roads | Roads Audio App',
     description: 'Explore the evolution of audio communication, from early radio innovations to the digital age with podcasts, leading up to the transformative Roads app.',
