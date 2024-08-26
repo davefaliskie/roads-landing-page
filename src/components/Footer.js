8f045404777b2a435627fc4e9069c08b0e08648c
Dave Faliskie <davefaliskie@gmail.com>
2024-08-26 14:07:16 -0400

    add socials to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import {
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+  IoLogoTiktok,
+  IoLogoTwitter,
+  IoLogoYoutube,
+} from "react-icons/io5";
 
 class Footer extends Component {
   render() {
@@ -18,6 +27,29 @@ class Footer extends Component {
           <Link href="mailto:Roads Audio<dave@roadsaudio.com>" className='btn btn-link'>
             Contact
           </Link>
+
+          <div className='mt-2 mb-3'>
+            <Link href="https://www.facebook.com/profile.php?id=61562204128726" target="_blank" className='social-icon mx-1'>
+              <IoLogoFacebook size={25}/>
+            </Link>
+
+            <Link href="https://www.instagram.com/roadsaudio/" target="_blank" className='social-icon mx-1'>
+              <IoLogoInstagram size={25}/>
+            </Link>
+
+            <Link href="https://www.linkedin.com/company/roads-audio/" target="_blank" className='social-icon mx-1'>
+              <IoLogoLinkedin size={25}/>
+            </Link>
+
+            <Link href="https://www.tiktok.com/@roadsaudio" target="_blank" className='social-icon mx-1'>
+              <IoLogoTiktok size={25}/>
+            </Link>
+
+            <Link href="https://www.youtube.com/@RoadsAudio" target="_blank" className='social-icon mx-1'>
+              <IoLogoYoutube size={25}/>
+            </Link>
+          </div>
+
           <p>Copyright © {current_year}, 1ManStartup LLC. All Rights Reserved.</p>
         </div>
       </div>
