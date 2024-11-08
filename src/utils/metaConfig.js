3cae6ef549bad7c00da42eb0e9643effb18a7466
Dave Faliskie <davefaliskie@gmail.com>
2024-11-08 14:37:55 -0500

    Change the headline

--- src/utils/metaConfig.js
+++ src/utils/metaConfig.js
@@ -1,7 +1,7 @@
 const metaConfig = {
   default: {
-    title: 'Roads Audio - Have Deeper Conversations',
-    description: 'Roads Audio brings you closer to your friends through deep, podcast-like conversations, letting you connect meaningfully on your time.',
+    title: 'Roads Audio - Private Podcasts With Friends',
+    description: 'Roads Audio brings you closer to your friends and family through meaningful, podcast-like conversations, allowing you to connect without scheduling conflicts.',
     image: 'https://roadsaudioassets.s3.amazonaws.com/Roads_Cover.png',
   },
   '/delete-account': {
