b7042fd50be7eb7e5831829a1cd7693a3bbc385f
Dave Faliskie <davefaliskie@gmail.com>
2025-11-06 19:29:21 +0000

    init the reddit pixel onload.

--- src/components/DynamicMeta.js
+++ src/components/DynamicMeta.js
@@ -56,9 +56,26 @@ const DynamicMeta = () => {
       {/* Reddit pixel */}
       <script
         dangerouslySetInnerHTML={{
-          __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_hynkkhfgc9y0');rdt('track', 'PageVisit');`
+          __html: `
+            !function(w,d){
+              if(!w.rdt){
+                var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};
+                p.callQueue=[];
+                var t=d.createElement("script");
+                t.src="https://www.redditstatic.com/ads/pixel.js";
+                t.async=true;
+                t.onload = function() {
+                  rdt('init', 'a2_hynkkhfgc9y0');
+                  rdt('track', 'PageVisit');
+                };
+                var s=d.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(t,s);
+              }
+            }(window,document);
+          `,
         }}
       />
+
 
     </Head>
   );
