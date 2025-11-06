28b84af587abcec3f0979a953ae59450dfc9c888
Dave Faliskie <davefaliskie@gmail.com>
2025-11-06 18:40:07 +0000

    add reddit pixel

--- src/components/DynamicMeta.js
+++ src/components/DynamicMeta.js
@@ -53,6 +53,13 @@ const DynamicMeta = () => {
         </>
       )}
 
+      {/* Reddit pixel */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_hynkkhfgc9y0');rdt('track', 'PageVisit');`
+        }}
+      />
+
     </Head>
   );
 };
