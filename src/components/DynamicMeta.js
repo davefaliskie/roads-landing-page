bb0e1e46ca1d7cf417608cd043a1b4d892db9630
Dave Faliskie <davefaliskie@gmail.com>
2026-01-17 20:23:49 -0300

    attempt rename of favicon

--- src/components/DynamicMeta.js
+++ src/components/DynamicMeta.js
@@ -30,7 +30,7 @@ const DynamicMeta = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
       
       {/* Favicon icons */}
-      <link rel="icon" href="/favicon/favicon.ico" />
+      <link rel="icon" href="/favicon/favicon2.ico" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
 
